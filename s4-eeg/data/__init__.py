5ff4fe298ee005eb96f198b26c6338f538cd5297
probablitycourse-cmyk <probablity.course@gmail.com>
2026-08-01 22:04:55 +0330

    Update __init__.py

--- s4-eeg/data/__init__.py
+++ s4-eeg/data/__init__.py
@@ -1,3 +1,15 @@
-from .dreamer import build_arrays, split_indices, make_loaders, TARGET_INDEX
+from .dreamer import (
+    build_arrays,
+    split_indices,
+    make_loaders,
+    make_loaders_per_subject,
+    TARGET_INDEX,
+)
 
-__all__ = ["build_arrays", "split_indices", "make_loaders", "TARGET_INDEX"]
+__all__ = [
+    "build_arrays",
+    "split_indices",
+    "make_loaders",
+    "make_loaders_per_subject",
+    "TARGET_INDEX",
+]
